f4d41d2466a9c42cc04f0fcf59ae045ac8155081
Thiago <thiagofoz@gmail.com>
2017-11-28 15:36:10 -0200

    Initial setup and heroku

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs'); //handlebars
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 hbs.registerPartials(__dirname+'/views/partials');
